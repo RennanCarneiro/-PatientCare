8c289e3583f579df56799c7aadfc5b496c769480
Rennan Carneiro <rennanbarros10@gmail.com>
2026-09-19 09:40:35 +0100

    Primeiro Commit

---
+++ src/test/java/com/rennancarneiro/patientcare/PatientcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rennancarneiro.patientcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
