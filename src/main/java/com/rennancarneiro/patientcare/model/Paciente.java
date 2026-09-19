1e99bfa43621eef06d419063605557e504f98087
Rennan Carneiro <rennanbarros10@gmail.com>
2026-09-19 10:40:15 +0100

    feat: adição inicial de model, DTOs, repository, service, e exception para gerenciamento do paciente

---
+++ src/main/java/com/rennancarneiro/patientcare/model/Paciente.java
@@ -0,0 +1,70 @@
+
+@Entity
+@Table(name = "pacientes")
+
+public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Enumerated(EnumType.STRING)  private Sexo sexo;
+    @Column(unique = true, nullable = false) private String cpf;
+    private LocalDate dataNascimento;
+    private String convenio;
+    private String telefone;
+
+    public Paciente() {
+
+    }
+     public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getConvenio() {
+        return convenio;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+}
