d623abc5139c0e75ed80b378b038f4a3075d6893
Rennan Carneiro <rennanbarros10@gmail.com>
2026-09-19 12:23:23 +0100

    feat: implementado a interface PacienteRepository extendendoo do jpaRepository

--- src/main/java/com/rennancarneiro/patientcare/repository/PacienteRepository.java
+++ src/main/java/com/rennancarneiro/patientcare/repository/PacienteRepository.java
@@ -1,3 +1,6 @@
-public class PatientRepository {
+import com.rennancarneiro.patientcare.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     
 }
